ec5b3e4584abb1e7fef30d2abf8ff8bdf4839427
Adrian Nagy <adrian.nagy@viablesystems.io>
2021-08-03 01:08:55 +0200

    WIP: config + tests

---
+++ src/configuration.rs
@@ -0,0 +1,111 @@
+use clap::{App, Arg};
+
+#[derive(Clone, Debug)]
+pub struct MonitoringTestEnvironment {
+    pub cpu_load: bool,
+
+    pub memory_load: Option<usize>,
+
+    pub network_and_io_load: Option<u64>,
+
+    pub test_cpu: Option<f64>,
+
+    pub test_memory: Option<u64>,
+
+    pub test_network_and_io: Option<u64>,
+}
+
+impl MonitoringTestEnvironment {
+    pub fn from_args() -> Self {
+        let app = monitoring_test_app();
+        let args = app.clone().get_matches();
+
+        Self {
+            cpu_load: args.is_present("cpu-load"),
+            memory_load: args
+                .value_of("memory-load")
+                .map(|memory_load| {
+                    memory_load
+                        .parse::<usize>()
+                        .expect("Was expecting NUM [u64]")
+                }),
+            network_and_io_load: args
+                .value_of("network-and-io-load")
+                .map(|network_and_io_load| {
+                    network_and_io_load
+                        .parse::<u64>()
+                        .expect("Was expecting NUM [u64]")
+                }),
+            test_cpu: args
+                .value_of("test-cpu")
+                .map(|target| {
+                    target
+                        .parse::<f64>()
+                        .expect("Was expecting FLOAT [f64]")
+                }),
+            test_memory: args
+                .value_of("test-memory")
+                .map(|target| {
+                    target
+                        .parse::<u64>()
+                        .expect("Was expecting NUM [u64]")
+                }),
+            test_network_and_io: args
+            .value_of("test-networking-and-io")
+            .map(|target| {
+                target
+                    .parse::<u64>()
+                    .expect("Was expecting NUM [u64]")
+            })
+        }
+    }
+}
+
+fn monitoring_test_app() -> App<'static, 'static> {
+
+    let app = App::new("Tezedge node monitoring app")
+        .version("1.7.0")
+        .author("SimpleStaking and the project contributors")
+        .setting(clap::AppSettings::AllArgsOverrideSelf)
+        .arg(
+            Arg::with_name("cpu-load")
+                .long("cpu-load")
+                .help("Launches the app with 100% cpu load")
+        )
+        .arg(
+            Arg::with_name("memory-load")
+                .long("memory-load")
+                .takes_value(true)
+                .value_name("NUM")
+                .help("Launches the app with the hardcoded load")
+        )
+        .arg(
+            Arg::with_name("network-and-io-load")
+                .long("network-and-io-load")
+                .takes_value(true)
+                .value_name("NUM")
+                .help("Launches the app with the newtwork and io load")
+        )
+        .arg(
+            Arg::with_name("test-cpu")
+                .long("test-cpu")
+                .takes_value(true)
+                .value_name("FLOAT")
+                .help("Launches test to assert cpu measurent")
+        )
+        .arg(
+            Arg::with_name("test-memory")
+                .long("test-memory")
+                .takes_value(true)
+                .value_name("NUM")
+                .help("Launches test to assert memory measurent")
+        )
+        .arg(
+            Arg::with_name("test-networking-and-io")
+                .long("test-networking-and-io")
+                .takes_value(true)
+                .value_name("NUM")
+                .help("Launches test to assert networking and io measurent")
+        );
+    app
+}
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,202 @@
-use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
+use std::convert::TryInto;
 use std::thread::sleep;
 use std::time::Duration;
 use std::mem;
 use std::env;
+use std::io::Write;
+use std::fs::File;
 
 pub mod rpc;
+pub mod configuration;
 
-use curl::easy::{Easy2, Handler, WriteError};
+use curl::easy::{Easy, Handler, WriteError};
+
+use configuration::MonitoringTestEnvironment;
 
 struct Collector(Vec<u8>);
 
 #[tokio::main]
 async fn main() {
 
-    let port = env::var("RPC_PORT").unwrap_or("18732".to_string()).parse::<u16>().unwrap();
-
-    rpc::spawn_rpc_server(port);
-    network_load();
-    sleep(Duration::MAX)
+    let env = MonitoringTestEnvironment::from_args();
+    
+    if env.cpu_load {
+        let port = env::var("RPC_PORT").unwrap_or("18732".to_string()).parse::<u16>().unwrap();
+        rpc::spawn_rpc_server(port);
+        cpu_load()
+    } else if let Some(mem_to_use) = env.memory_load {
+        let port = env::var("RPC_PORT").unwrap_or("18732".to_string()).parse::<u16>().unwrap();
+        rpc::spawn_rpc_server(port);
+        memory_load(mem_to_use)
+    } else if let Some(network_and_io_load_to_use) = env.network_and_io_load {
+        let port = env::var("RPC_PORT").unwrap_or("18732".to_string()).parse::<u16>().unwrap();
+        rpc::spawn_rpc_server(port);
+        network_and_io_load(network_and_io_load_to_use);
+    } else if let Some(cpu_target) = env.test_cpu {
+        test_cpu(cpu_target).await;
+    } else if let Some(memory_target) = env.test_memory {
+        test_memory(memory_target).await;
+    } else if let Some(io_network_target) = env.test_network_and_io {
+        test_network_and_io(io_network_target).await;
+    }
 }
 
 fn cpu_load() {
     loop {}
 }
 
-fn memory_load() {
-    // 1 GB
-    const ALLOCATE: usize = 1_073_741_824;
+fn memory_load(mem_to_use: usize) {
+    // From std::mem doc: In general, the size of a type is not stable across compilations, but specific types such as primitives are.
+    // u8 should always be a size of 1 Byte
 
-    // 512 MB
-    // const ALLOCATE: usize = 1_073_741_824 / 2;
+    println!("=== MEMORY SIMULATION STARTED ===");
+    println!("    ALLOCATING {} BYTES", mem_to_use);
 
-    // 2 GB
-    // const ALLOCATE: usize = 1_073_741_824 * 2;
+    let sizeof_bool = mem::size_of::<u8>();
+    let count = mem_to_use / sizeof_bool;
+    let mut artificial_memory_load: Vec<u8> = Vec::with_capacity(count);
+    artificial_memory_load.resize(count, 0);
 
-    // From std::mem doc: In general, the size of a type is not stable across compilations, but specific types such as primitives are
-    // bool should always be a size of 1 Byte
-    let sizeof_bool = mem::size_of::<bool>();
-    let count = ALLOCATE / sizeof_bool;
-    let mut artificial_memory_load: Vec<bool> = Vec::with_capacity(count);
-
-    for _ in 0..count {
-        artificial_memory_load.push(true)
-    }
-    sleep(Duration::MAX)
+    println!("    MEMORY LOADED, SLEEPING...");
+    sleep(Duration::MAX);
 }
 
-fn network_load() {
+fn network_and_io_load(network_and_io_load_to_use: u64) {
     impl Handler for Collector {
         fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
             self.0.extend_from_slice(data);
             Ok(data.len())
         }
     }
+    let mut file = File::create("downloaded.file").unwrap();
 
-    let mut easy = Easy2::new(Collector(Vec::new()));
+    // let mut easy = Easy2::new(Collector(Vec::new()));
+    let mut easy = Easy::new();
     easy.get(true).unwrap();
     // TODO: change this to something more appropriate
     easy.url("http://65.21.165.81:8080/rt-kernel/linux-image-5.10.41-rt42-dbg_5.10.41-rt42-1_amd64.deb").unwrap();
-    easy.max_recv_speed(102400).unwrap();
-    easy.low_speed_limit(102400).unwrap();
+    easy.max_recv_speed(network_and_io_load_to_use).unwrap();
+    easy.low_speed_limit(network_and_io_load_to_use.try_into().unwrap()).unwrap();
     println!("Download starting");
+
+    easy.write_function(move |data| {
+        file.write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+
     easy.perform().unwrap();
+
 }
 
-fn io_load() {
-    
+async fn test_cpu(target: f64) {
+    println!("=== TESTING NODE CPU MEASUREMENT ===\n");
+
+    let error_margin = 2.0;
+
+    println!("   TARGET: {}%", target);
+    println!("   ERROR MARGIN: {}%\n", error_margin);
+
+    let res = get_latest_measurement().await;
+
+    if let Some(cpu_data) = res[0]["cpu"]["node"]["collective"].as_f64() {
+        println!("   CPU at: {}%\n", cpu_data);
+
+        // Make sure the measurement is withing the defined interval (with the error margin)
+        assert!(target + error_margin >= cpu_data);
+        assert!(target - error_margin <= cpu_data);
+
+        println!("=== OK ===");
+
+    } else {
+        panic!("Test failed: No cpu data found in measurements")
+    }
+}
+
+async fn test_memory(target: u64) {
+    println!("=== TESTING NODE MEMORY MEASUREMENT ===\n");
+
+    // we need to take into consideration all the other memory allocation (stacks, warp server...)
+    // 30 MB
+    let error_margin = 31_457_280;
+
+    println!("   TARGET: {}MB", bytes_to_megabytes(target));
+    println!("   ERROR MARGIN: {}MB\n", bytes_to_megabytes(error_margin));
+
+    let res = get_latest_measurement().await;
+
+    if let Some(memory_data) = res[0]["memory"]["node"].as_u64() {
+        // DEBUG
+        println!("   Memory at: {}MB\n", bytes_to_megabytes(memory_data));
+        assert!(target + error_margin >= memory_data);
+        assert!(target - error_margin <= memory_data);
+
+        println!("=== OK ===");
+
+    } else {
+        panic!("Test failed: No cpu data found in measurements")
+    }
+}
+
+async fn test_network_and_io(target: u64) {
+    println!("=== TESTING NODE IO ===\n");
+
+    // there could be many bottlenecks to networking, give it a 20 KB/s error_margin
+    // 30 KB/s
+    let error_margin = 30_720;
+
+    println!("   TARGET: {}KB/s", bytes_to_kilobytes(target));
+    println!("   ERROR MARGIN: {}KB/s\n", bytes_to_kilobytes(error_margin));
+
+    let res = get_latest_measurement().await;
+
+    if let Some(io_data) = res[0]["io"]["node"]["writtenBytesPerSec"].as_u64() {
+        // DEBUG
+        println!("   DISK WRITE at: {}KB/s\n", bytes_to_kilobytes(io_data));
+        assert!(target + error_margin >= io_data);
+        assert!(target - error_margin <= io_data);
+
+        println!("=== OK ===");
+
+    } else {
+        panic!("Test failed: No io data found in measurements")
+    }
+
+    println!("=== TESTING NODE NETWORKING ===\n");
+
+
+    let error_margin = 61_440;
+    println!("   TARGET: {}KB/s", bytes_to_kilobytes(target));
+    println!("   ERROR MARGIN: {}KB/s\n", bytes_to_kilobytes(error_margin));
+
+    if let Some(network_data) = res[0]["network"]["receivedBytesPerSec"].as_u64() {
+        // DEBUG
+        println!("   NETWORK RECIEVED at: {}KB/s\n", bytes_to_kilobytes(network_data));
+        assert!(target + error_margin >= network_data);
+        assert!(target - error_margin <= network_data);
+
+        println!("=== OK ===");
+
+    } else {
+        panic!("Test failed: No network data found in measurements")
+    }
+}
+
+async fn get_latest_measurement() -> serde_json::Value {
+    match reqwest::get(
+        "http://127.0.0.1:38732/resources/tezedge?limit=1"
+    ).await
+    {
+        Ok(result) => return result.json().await.unwrap_or_default(),
+        Err(e) => {
+            panic!("Measurement request error: {}", e);
+        }
+    };
+}
+
+fn bytes_to_megabytes(bytes: u64) -> u64 {
+    bytes / 1024 / 1024
+}
+
+fn bytes_to_kilobytes(bytes: u64) -> u64 {
+    bytes / 1024
 }
